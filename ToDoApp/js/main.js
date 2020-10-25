6df420601d7b34e845475b4d3ade00e5cf9699f6
Shogo Yoshizawa <momoten0141@gmail.com>
2020-10-25 20:08:46 +0900

    [add]保存機能追加

--- ToDoApp/js/main.js
+++ ToDoApp/js/main.js
@@ -7,11 +7,28 @@ function newTodo(todo){
     todoList.innerHTML += `<li class ="todo"><p class="txt">${todo}</p><i class="delete far fa-trash-alt"></i></li>`;
 }
 
+function getCookie(todo){
+    newTodo(todo);
+}
+
+function setCookie(todo){
+    Cookies.set(todo, todo, { expires: 1/144 });
+}
+
+function deleteCookie(todo){
+    Cookies.remove(todo);
+}
+
+for (var todo in Cookies.get()) {
+    getCookie(todo);
+}
+
 add.addEventListener('submit',e => {
     e.preventDefault();
 
     const todo = add.querySelector('.txt').value.trim();
     if (todo.length) {
+        setCookie(todo);
         newTodo(todo);
         add.reset();
     }
@@ -19,6 +36,8 @@ add.addEventListener('submit',e => {
 
 todoList.addEventListener('click',e => {
     if (e.target.classList.contains('delete')){
+        const todo = e.target.parentElement.querySelector('.txt').textContent;
+        deleteCookie(todo);
         e.target.parentElement.remove();
     }
 });
