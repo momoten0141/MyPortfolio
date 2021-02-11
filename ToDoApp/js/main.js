e16381f0b3dfdaaf12f96e31b6ee55bee9125b20
Shogo Yoshizawa <momoten0141@gmail.com>
2021-02-12 01:08:10 +0900

    [modify]checkedを維持できるよう修正

--- ToDoApp/js/main.js
+++ ToDoApp/js/main.js
@@ -1,13 +1,19 @@
-const newTodo = todo=>{
+const newTodo = (todo,checked)=>{
     // 新しくtodoを生成
-    $('#todoList').prepend(`<li class="todo-item"><span class="todo"><input class="checkbox" type="checkbox"><span class="todo-txt">${todo}</span></span><span class="far fa-edit"></span></li>`);
+    if(checked){
+        $('#todoList').prepend(`<li class="todo-item"><span class="todo"><input class="checkbox" type="checkbox" checked="checked"><span class="todo-txt line-through">${todo}</span></span><span class="far fa-trash-alt"></span></li>`);
+    }else{
+        $('#todoList').prepend(`<li class="todo-item"><span class="todo"><input class="checkbox" type="checkbox"><span class="todo-txt">${todo}</span></span><span class="far fa-edit"></span></li>`);
+    }
+
     // 生成したtodoにdata-todo-idを付与
     $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
 }
 
 // クッキーをセット
-const setCookie = (id,todo)=>{
-    Cookies.set(id, todo, { expires: 1/144 });
+const setCookie = (id,todo,checked)=>{
+    const data = JSON.stringify({todo: todo, checked: checked});
+    Cookies.set(id, data, { expires: 1/144 });
 }
 
 // 全てのクッキーを削除
@@ -20,12 +26,12 @@ const deleteAllCookie = ()=>{
 // クッキー保存分todoを生成
 const setAllTodo = ()=>{
     for(let id in Cookies.get()){
-        const todo = Cookies.get(id);
-        newTodo(todo);
+        const data = JSON.parse(Cookies.get(id));
+        const todo = data.todo;
+        const checked = data.checked;
+        newTodo(todo,checked);
     }
 }
-
-
 
 // クッキーに保存されている全てのtodoを生成
 setAllTodo();
@@ -38,7 +44,8 @@ $('#addTodo').submit(e=>{
     if(todo.length){
         newTodo(todo);
         const id = $('#todoList > .todo-item:first').attr('data-todo-id');
-        setCookie(id,todo);
+        const checked = false;
+        setCookie(id,todo,checked);
         $('#addTodo')[0].reset();
     }
 });
@@ -53,12 +60,15 @@ $(document).on('click','.fa-trash-alt',function(){
     const todoNum = $('#todoList > .todo-item').length;
     for(let i=1;i<=todoNum;i++){
         let todo;
-        if($(`#todoList > .todo-item:nth-last-child(${i})`).text()){
+        let checked;
+         if($(`#todoList > .todo-item:nth-last-child(${i})`).text()){
             todo = $(`#todoList > .todo-item:nth-last-child(${i})`).text();
+            checked = $(`#todoList > .todo-item:nth-last-child(${i})`).find('.checkbox').prop("checked");
         }else{
             todo= $.trim($(`#todoList > .todo-item:nth-last-child(${i})`).find('.todo-txt').val());
+            checked = false;
         }
-        setCookie(i,todo);
+        setCookie(i,todo,checked);
     }
     // クッキーを基にtodoを再生成
     $('#todoList > ').remove();
@@ -68,7 +78,9 @@ $(document).on('click','.fa-trash-alt',function(){
 // 編集アイコンをクリックした時の処理
 $(document).on('click','.fa-edit',function(){
     const id = $(this).parent().attr('data-todo-id');
-    const todo = Cookies.get(id);
+    // const todo = Cookies.get(id);
+    const data = JSON.parse(Cookies.get(id));
+    const todo = data.todo; 
     $(this).toggleClass('fa-edit');
     $(this).toggleClass('fa-check-circle');
     $(this).parent().find('.todo').replaceWith(`<span class="todo todo-edit"><input class="todo-txt" type="text" value="${todo}"></span>`);
@@ -78,7 +90,7 @@ $(document).on('click','.fa-edit',function(){
 $(document).on('click','.fa-check-circle',function(){
     const todo= $.trim($(this).parent().find('.todo-txt').val());
     const id = $(this).parent().attr('data-todo-id');
-    setCookie(id,todo);
+    setCookie(id,todo,false);
     $(this).toggleClass('fa-edit');
     $(this).toggleClass('fa-check-circle');
     $(this).parent().find('.todo').replaceWith(`<span class="todo"><input class="checkbox" type="checkbox"><span class="todo-txt">${todo}</span></span>`);
@@ -89,4 +101,7 @@ $(document).on('click','.checkbox',function(){
     $(this).parents('.todo-item').find('.far').toggleClass('fa-trash-alt');
     $(this).parents('.todo-item').find('.far').toggleClass('fa-edit');
     $(this).parent().find('.todo-txt').toggleClass('line-through');
+    const id = $(this).parents('.todo-item').attr('data-todo-id');
+    const todo = $(this).parent().find('.todo-txt').text();
+    setCookie(id,todo,true);
 });
