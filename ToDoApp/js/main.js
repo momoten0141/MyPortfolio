f7d2da951c3699cb72907ecd1ce2e21078b93390
Shogo Yoshizawa <momoten0141@gmail.com>
2021-02-11 14:53:36 +0900

    [modify]コメント追加

--- ToDoApp/js/main.js
+++ ToDoApp/js/main.js
@@ -1,52 +1,63 @@
 const newTodo = todo=>{
+    // 新しくtodoを生成
     $('#todoList').prepend(`<li class="todo-item"><span class="todo"><input class="checkbox" type="checkbox"><span class="todo-txt">${todo}</span></span><span class="far fa-edit"></span></li>`);
+    // 生成したtodoにdata-todo-idを付与
+    $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
 }
 
+// クッキーをセット
 const setCookie = (id,todo)=>{
     Cookies.set(id, todo, { expires: 1/144 });
 }
 
+// 全てのクッキーを削除
 const deleteAllCookie = ()=>{
     for(let id in Cookies.get()){
         Cookies.remove(id);
     }
 }
 
-// 保存数分todoを生成
-for(let id in Cookies.get()){
-    const todo = Cookies.get(id);
-    newTodo(todo);
-    $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
+// クッキー保存分todoを生成
+const setAllTodo = ()=>{
+    for(let id in Cookies.get()){
+        const todo = Cookies.get(id);
+        newTodo(todo);
+    }
 }
+
+
+
+// クッキーに保存されている全てのtodoを生成
+setAllTodo();
 
 // todoを入力した時の処理
 $('#addTodo').submit(e=>{
     e.preventDefault();
+    // todoを取得
     const todo = $.trim($('#enterTodo').val());
     if(todo.length){
         newTodo(todo);
-        $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
-        const idNum = $('#todoList > .todo-item:first').attr('data-todo-id');
-        setCookie(idNum,todo);
+        const id = $('#todoList > .todo-item:first').attr('data-todo-id');
+        setCookie(id,todo);
         $('#addTodo')[0].reset();
     }
 });
 
 // ゴミ箱アイコンをクリックした時の処理
 $(document).on('click','.fa-trash-alt',function(){
+    // 洗濯したtodoを削除
     $(this).parent().remove();
+    // 全てのクッキーを削除
     deleteAllCookie();
+    // 全てのtodoを取得してクッキーセット
     const todoNum = $('#todoList > .todo-item').length;
     for(let i=1;i<=todoNum;i++){
         const todo = $(`#todoList > .todo-item:nth-last-child(${i})`).text();
         setCookie(i,todo);
     }
+    // クッキーを基にtodoを再生成
     $('#todoList > ').remove();
-    for(let id in Cookies.get()){
-        const todo = Cookies.get(id);
-        newTodo(todo);
-        $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
-    }
+    setAllTodo();
 });
 
 // 編集アイコンをクリックした時の処理
