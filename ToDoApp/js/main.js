4f0157b44c4987e2ee1a071c0b0a35140735b852
Shogo Yoshizawa <momoten0141@gmail.com>
2021-02-11 12:28:29 +0900

    [modify]todoをidで管理

--- ToDoApp/js/main.js
+++ ToDoApp/js/main.js
@@ -2,20 +2,21 @@ const newTodo = todo=>{
     $('#todoList').prepend(`<li class="todo-item"><span class="todo"><input class="checkbox" type="checkbox"><span class="todo-txt">${todo}</span></span><span class="far fa-edit"></span></li>`);
 }
 
-const setCookie = todo=>{
-    Cookies.set(todo, todo, { expires: 1/144 });
+const setCookie = (id,todo)=>{
+    Cookies.set(id, todo, { expires: 1/144 });
 }
 
-const deleteCookie = todo=>{
-    Cookies.remove(todo);
+const deleteAllCookie = ()=>{
+    for(let id in Cookies.get()){
+        Cookies.remove(id);
+    }
 }
-
-let beforeCookie;
-
 
 // 保存数分todoを生成
-for(let todo in Cookies.get()){
+for(let id in Cookies.get()){
+    const todo = Cookies.get(id);
     newTodo(todo);
+    $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
 }
 
 // todoを入力した時の処理
@@ -23,23 +24,35 @@ $('#addTodo').submit(e=>{
     e.preventDefault();
     const todo = $.trim($('#enterTodo').val());
     if(todo.length){
-        setCookie(todo);
         newTodo(todo);
+        $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
+        const idNum = $('#todoList > .todo-item:first').attr('data-todo-id');
+        setCookie(idNum,todo);
         $('#addTodo')[0].reset();
     }
 });
 
 // ゴミ箱アイコンをクリックした時の処理
 $(document).on('click','.fa-trash-alt',function(){
-    const todo = $(this).parent().find('.todo-txt').text();
-    deleteCookie(todo);
     $(this).parent().remove();
+    deleteAllCookie();
+    const todoNum = $('#todoList > .todo-item').length;
+    for(let i=1;i<=todoNum;i++){
+        const todo = $(`#todoList > .todo-item:nth-last-child(${i})`).text();
+        setCookie(i,todo);
+    }
+    $('#todoList > ').remove();
+    for(let id in Cookies.get()){
+        const todo = Cookies.get(id);
+        newTodo(todo);
+        $('#todoList > .todo-item:first').attr('data-todo-id',`${$('#todoList > .todo-item').length}`);
+    }
 });
 
 // 編集アイコンをクリックした時の処理
 $(document).on('click','.fa-edit',function(){
-    const todo = $(this).parent().find('.todo-txt').text();
-    beforeCookie = todo;
+    const id = $(this).parent().attr('data-todo-id');
+    const todo = Cookies.get(id);
     $(this).toggleClass('fa-edit');
     $(this).toggleClass('fa-check-circle');
     $(this).parent().find('.todo').replaceWith(`<span class="todo todo-edit"><input class="todo-txt" type="text" value="${todo}"></span>`);
@@ -48,15 +61,12 @@ $(document).on('click','.fa-edit',function(){
 // 編集画面でチェックを入れた時の処理
 $(document).on('click','.fa-check-circle',function(){
     const todo= $.trim($(this).parent().find('.todo-txt').val());
-    deleteCookie(beforeCookie);
-    setCookie(todo);
+    const id = $(this).parent().attr('data-todo-id');
+    setCookie(id,todo);
     $(this).toggleClass('fa-edit');
     $(this).toggleClass('fa-check-circle');
     $(this).parent().find('.todo').replaceWith(`<span class="todo"><input class="checkbox" type="checkbox"><span class="todo-txt">${todo}</span></span>`);
 });
-
-
-
 
 // チェックボックスにチェックを入れた時の処理
 $(document).on('click','.checkbox',function(){
