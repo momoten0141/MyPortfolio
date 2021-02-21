bca5afc43cf5cee2d75164d0c50301189c18ea54
Shogo Yoshizawa <momoten0141@gmail.com>
2021-02-22 06:56:07 +0900

    [add]アンカーに対応

--- UIModuleLibrary/root/common/js/component.js
+++ UIModuleLibrary/root/common/js/component.js
@@ -1,3 +1,60 @@
+$(function(){
+// アンカーによるスクロール処理
+const modules = ['.c-accordion','.c-tabs'];
+const handleHash = ()=>{
+    const hash = location.hash;
+    if(hash !== ''){
+        for(let i=0; i<modules.length; i++){
+            const moduleId = $(hash).parents(modules[i]).attr('data-module-id');
+            let position;
+            switch(moduleId){
+                case 'A01':
+                    $(hash).parents('.c-accordion').find('.c-accordion__toggle').text('CLOSE');
+                    $(hash).parents('.c-accordion').find('.c-accordion__body').slideDown();
+                    $(hash).parents('.c-accordion').addClass('c-accordion-active');
+                    position = $(hash).offset().top;
+                    setTimeout(function(){
+                        $(window).scrollTop(position);
+                    },400);
+                break;
+                case 'A02':
+                    $(hash).parents('.c-accordion').find('.c-accordion__toggle').text('CLOSE');
+                    $(hash).parents('.c-accordion').find('.c-accordion__body').slideDown();
+                    $(hash).parents('.c-accordion').addClass('c-accordion-active');
+                    position = $(hash).offset().top;
+                    setTimeout(function(){
+                        $(window).scrollTop(position);
+                    },400);
+                break;
+                case 'B01':
+                    if($(hash).hasClass('c-tab')){
+                        const index = $(hash).index();
+                        $(hash).parents('.c-tabs__head').find('.c-tab-active').toggleClass('c-tab-active');
+                        $(hash).toggleClass('c-tab-active');
+                        $(hash).parents('.c-tabs').find('.c-tab__body-active').toggleClass('c-tab__body-active');
+                        $(hash).parents('.c-tabs').find('.c-tab__body').eq(index).toggleClass('c-tab__body-active');
+                    }else{
+                        const index = $(hash).parents('.c-tab__body').index();
+                        $(hash).parents('.c-tabs').find('.c-tabs__head').find('.c-tab-active').toggleClass('c-tab-active');
+                        $(hash).parents('.c-tabs').find('.c-tab').eq(index).toggleClass('c-tab-active');
+                        $(hash).parents('.c-tabs__body').find('.c-tab__body-active').toggleClass('c-tab__body-active');
+                        $(hash).parents('.c-tab__body').toggleClass('c-tab__body-active');
+                    }
+                    position = $(hash).offset().top;
+                    $(window).scrollTop(position);
+                break;
+                default:
+            }
+        }
+    }
+}
+
+handleHash();
+
+$(window).on('hashchange', () => {
+    handleHash();
+});
+
 // ------------------------------------------
 // アコーディオン
 // ------------------------------------------
@@ -6,11 +63,11 @@
 // ------------------------------------------
 $('.c-accordion__toggle').on('click',function(){
     if($(this).parents('.c-accordion').hasClass('c-accordion-active')){
-        $(this).text('open');
+        $(this).text('OPEN');
         $(this).parents('.c-accordion').find('.c-accordion__body').slideUp();
         $(this).parents('.c-accordion').toggleClass('c-accordion-active');
     }else{
-        $(this).text('close');
+        $(this).text('CLOSE');
         $(this).parents('.c-accordion').find('.c-accordion__body').slideDown();
         $(this).parents('.c-accordion').toggleClass('c-accordion-active');
     }
@@ -32,4 +89,6 @@ $('.c-tab').on('click',function(){
         $(this).parents('.c-tabs').find('.c-tab__body-active').toggleClass('c-tab__body-active');
         $(this).parents('.c-tabs').find('.c-tab__body').eq(index).toggleClass('c-tab__body-active');
     }
+});
+
 });
